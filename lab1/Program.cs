d70cb6c9d259e230f051a18939f7518da2b53e61
Di <b.dima1916@gmail.com>
2023-02-21 19:35:11 +0200

    add task 1

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -10,6 +10,22 @@ namespace lab1
     {
         static void Main(string[] args)
         {
+            Task_1();
+        }
+        static void Task_1()
+        {
+            int octal = 0, i = 1;
+            Console.Write("Enter a decimal number: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+            int temp = decimalNumber;
+            while (temp > 0)
+            {
+                int remainder = temp % 8;
+                octal += remainder * i;
+                i *= 10;
+                temp /= 8;
+            }
+            Console.WriteLine("The octal equivalent of {0} is {1}.", decimalNumber, octal);
         }
     }
 }
